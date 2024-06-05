04e0b0e40ac5deff4a2311f49f8f13ca40a866fa
Sungho Woo <wsh@robotis.com>
2024-06-05 15:00:16 +0900

    removed
    Signed-off-by: Sungho Woo <wsh@robotis.com>

--- nav2_bringup/bringup/launch/bringup_launch.py
+++ nav2_bringup/bringup/launch/bringup_launch.py
@@ -33,11 +33,11 @@ def generate_launch_description():
     # Create the launch configuration variables
     namespace = LaunchConfiguration('namespace')
     use_namespace = LaunchConfiguration('use_namespace')
-    slam = LaunchConfiguration('slam')
-    map_yaml_file = LaunchConfiguration('map')
+    # slam = LaunchConfiguration('slam')
+    # map_yaml_file = LaunchConfiguration('map')
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
-    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
+    # default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     autostart = LaunchConfiguration('autostart')
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
@@ -72,12 +72,12 @@ def generate_launch_description():
         default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
-    declare_bt_xml_cmd = DeclareLaunchArgument(
-        'default_bt_xml_filename',
-        default_value=os.path.join(
-            get_package_share_directory('nav2_bt_navigator'),
-            'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
-        description='Full path to the behavior tree xml file to use')
+    # declare_bt_xml_cmd = DeclareLaunchArgument(
+    #     'default_bt_xml_filename',
+    #     default_value=os.path.join(
+    #         get_package_share_directory('nav2_bt_navigator'),
+    #         'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+    #     description='Full path to the behavior tree xml file to use')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -133,7 +133,7 @@ def generate_launch_description():
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_autostart_cmd)
-    ld.add_action(declare_bt_xml_cmd)
+    #ld.add_action(declare_bt_xml_cmd)
 
     # Add the actions to launch all of the navigation nodes
     ld.add_action(bringup_cmd_group)
--- nav2_bringup/bringup/launch/navigation_launch.py
+++ nav2_bringup/bringup/launch/navigation_launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
-    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
+    # default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     map_subscribe_transient_local = LaunchConfiguration('map_subscribe_transient_local')
 
     # lifecycle_nodes = ['controller_server',
@@ -53,11 +53,11 @@ def generate_launch_description():
                   ('/tf_static', 'tf_static')]
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'default_bt_xml_filename': default_bt_xml_filename,
-        'autostart': autostart,
-        'map_subscribe_transient_local': map_subscribe_transient_local}
+    # param_substitutions = {
+    #     'use_sim_time': use_sim_time,
+    #     'default_bt_xml_filename': default_bt_xml_filename,
+    #     'autostart': autostart,
+    #     'map_subscribe_transient_local': map_subscribe_transient_local}
 
     configured_params = RewrittenYaml(
             source_file=params_file,
@@ -86,12 +86,12 @@ def generate_launch_description():
             default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
             description='Full path to the ROS2 parameters file to use'),
 
-        DeclareLaunchArgument(
-            'default_bt_xml_filename',
-            default_value=os.path.join(
-                get_package_share_directory('nav2_bt_navigator'),
-                'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
-            description='Full path to the behavior tree xml file to use'),
+        # DeclareLaunchArgument(
+        #     'default_bt_xml_filename',
+        #     default_value=os.path.join(
+        #         get_package_share_directory('nav2_bt_navigator'),
+        #         'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+        #     description='Full path to the behavior tree xml file to use'),
 
         DeclareLaunchArgument(
             'map_subscribe_transient_local', default_value='false',
